736e528a3f2c277fc9b06174fad392dda51ab03e
Lyudmila Kachan <ludkakachan@gmail.com>
2022-05-20 21:30:38 +0300

    add classwork.py

---
+++ lessons_03/classwork.py
@@ -0,0 +1,64 @@
+In [1]: x1=17/2*3+2
+
+In [2]: x1
+Out[2]: 27.5
+
+In [3]: x2=2+17/2*3
+
+In [4]: x2
+Out[4]: 27.5
+
+In [5]: x3=19%4+15/2*3
+
+In [6]: x3
+Out[6]: 25.5
+
+In [7]: x4=(15+6)-10*4
+
+In [8]: x4
+Out[8]: -19
+
+In [9]: x=17/2%2*3
+
+In [10]: x
+Out[10]: 1.5
+
+In [11]: x1=17/2*(3+2)
+
+In [12]: x1
+Out[12]: 42.5
+
+In [14]: x2=(2+17)/2*3
+
+In [15]: x2
+Out[15]: 28.5
+
+In [16]: age_d=26
+
+In [17]: age_o=25
+
+In [18]: age_y=25
+
+In [19]: age_d+age_o+age_y
+Out[19]: 76
+
+In [20]: (age_d+age_o+age_y)/3
+Out[20]: 25.333333333333332
+
+In [21]: my_list=[1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+
+In [22]: me_set=set(my_list)
+
+In [25]: len(my_list)-len(me_set)
+Out[25]: 3
+
+In [27]: my_list[3:6][::-1]
+Out[27]: ['python', 5.0, 2]
+
+In [28]: lenght=5
+
+In [51]: sqr_list=[(lenght+lenght)*2,lenght*lenght,((lenght**2*2))**0.5]
+
+In [52]: sqr_list
+Out[52]: [20, 25, 7.0710678118654755]
+
